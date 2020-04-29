410930e06a70bc98eea9ac02fd2c9d7ac9fc62fa
Etiendo <32451923+Etiendo@users.noreply.github.com>
2020-04-29 20:31:47 +0200

    Initialize react native app

---
+++ Component/Map.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class Map extends React.Component {
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <View style={styles.text_container}>
+                    <Text>Trouvez votre échoppe !</Text>
+                </View>
+                <View style={styles.map_container}>
+                    <Text>Carte à venir prochainement ! </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+    },
+    text_container: {
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    map_container: {
+        flex: 4,
+        backgroundColor: 'red',
+        alignContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default Map
