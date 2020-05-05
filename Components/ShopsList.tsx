fdba953910ec7add41a7a863860e7a866b31a68f
Etiendo <32451923+Etiendo@users.noreply.github.com>
2020-05-05 11:31:15 +0200

    Initialize a basic list for shops

--- Components/ShopsList.tsx
+++ Components/ShopsList.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native'
 import { getAllShops } from '../API/chesseApi'
 
 class ShopsList extends React.Component {
     constructor(props: any, private searchedText: string, private page: number, private totalPages: number) {
         super(props)
-        this.searchedText
-        this.page = 0
-        this.totalPages = 0
         this.state = {
             shops: [],
             isLoading: false
@@ -16,9 +13,25 @@ class ShopsList extends React.Component {
     }
 
     _loadShops() {
-        getAllShops(1).then(data => {
-            console.log('_loadShops', data)
+        this.setState({
+            isLoading: true
         })
+        getAllShops().then(data => {
+            this.setState({
+                shops: data.results,
+                isLoading: false
+            })
+        })
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
     }
 
     componentDidMount() {
@@ -28,7 +41,14 @@ class ShopsList extends React.Component {
     render() {
         return (
             <View style={styles.main_container}>
-                <Text>Shops list works !</Text>
+                <FlatList
+                    style={styles.list}
+                    data={this.state.shops}
+                    keyExtractor={(item: any) => item.id.toString()}
+                    renderItem={({ item }) => (<Text>{item.name}</Text>)}
+                />
+
+                {this._displayLoading()}
             </View>
         )
     }
@@ -37,7 +57,19 @@ class ShopsList extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1
-    }
+    },
+    list: {
+        flex: 1
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 10,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 })
 
 export default ShopsList
